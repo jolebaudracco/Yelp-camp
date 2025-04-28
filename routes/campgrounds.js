784e5840b165f5802c66413a29115cd8b22b5e89
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-04-28 18:09:02 -0300

    corroborating that the campgrounds are removed and are not displayed again

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -36,11 +36,19 @@ router.post('/', validateCampground, catchAsync(async(req, res, next) => {
 router.get('/:id', catchAsync(async(req, res) => {
     console.log("🚀JB ~ router.get ~ res:", res)
     const campground = await Campground.findById(req.params.id).populate('reviews')
+    if(!campground){
+        req.flash('error', 'cannot find that campground!');
+        return res.redirect('/campgrounds')
+    }
     res.render('campgrounds/show', { campground });
 }));
 
 router.get('/:id/edit', catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id)
+    if(!campground){
+        req.flash('error', 'cannot find that campground!');
+        return res.redirect('/campgrounds')
+    }
     res.render('campgrounds/edit', { campground });
 }));
 
