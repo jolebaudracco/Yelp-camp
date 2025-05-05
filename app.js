07e6362e579987e5109742f65fe07bbc34010265
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-05-05 17:34:36 -0300

    checking users to see actions in the web for verification

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ const sessionConfig = {
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
-
 }
+
 app.use(session(sessionConfig));
 app.use(flash());
 
