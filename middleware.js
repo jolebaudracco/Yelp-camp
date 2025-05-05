597afaaa96f9daaf094ba28df44ef8adee13512f
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-05-05 14:14:11 -0300

    Saves the URL the user tried to visit

--- middleware.js
+++ middleware.js
@@ -1,8 +1,18 @@
 module.exports.isLoggedIn = (req, res, next) => {
     console.log("REQ.USER...", req.user);
-    if(!req.isAuthenticated()){
+    if (!req.isAuthenticated()) {
+        // Guarda la URL que el usuario intentó visitar
+        req.session.returnTo = req.originalUrl; // <-- ESTA LÍNEA ES CLAVE
         req.flash('error', 'You must be signed in first!');
         return res.redirect('/login');
     }
-    next()
-}
+    next();
+};
+
+// NUEVO MIDDLEWARE: guarda el returnTo en res.locals
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+};
