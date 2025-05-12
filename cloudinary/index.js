24733158f345294b7b89f8f50f7979293b98c9cf
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-05-12 09:03:07 -0300

    Modifications in the word: "Cloudinary"

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDYNARY_CLOUD_NAME,
-    api_key: process.env.CLOUDYNARY_KEY,
-    api_secret: process.env.CLOUDYNARY_SECRET
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
 });
 
 const storage = new CloudinaryStorage({
