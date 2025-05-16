f864d536c9520a472e97fca9d078b05a4740c763
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-05-16 13:52:44 -0300

    changes in the imgs because they weren't recognized by my db

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -11,14 +11,14 @@ const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
+    .post(isLoggedIn, upload.array('images'), validateCampground, catchAsync(campgrounds.createCampground))
     
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
+    .put(isLoggedIn, isAuthor, upload.array('images'), validateCampground, catchAsync(campgrounds.updateCampground))
     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
 
