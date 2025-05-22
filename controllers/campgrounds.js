d49f730d1ed2d21e23c6aef8f89a213285125aa0
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-05-22 13:46:44 -0300

    adding the maps functionality to my campground page

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,6 @@
 const Campground = require('../models/campground');
+const maptilerClient = require("@maptiler/client");
+maptilerClient.config.apiKey = process.env.MAPTILER_API_KEY;
 const { cloudinary } = require('../cloudinary');
 
 module.exports.index = async (req, res) => {
@@ -11,13 +13,26 @@ module.exports.renderNewForm = (req, res) => {
 }
 
 module.exports.createCampground = async(req, res, next) => {
+
+    const fetch = (await import('node-fetch')).default;
+    maptilerClient.config.fetch = fetch;
+
+    const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 });
+
+    if (!geoData.features || geoData.features.length === 0) {
+        req.flash('error', 'Location not found. Please enter a valid location.');
+        return res.redirect('/campgrounds/new');
+    }
+
+    
     const campground = new Campground(req.body.campground);
+    campground.geometry = geoData.features[0].geometry;
     campground.images = req.files.map(f => ({url: f.path, filename: f.filename})) 
     campground.author = req.user._id;
     await campground.save();
     console.log(campground);
     req.flash('success', 'Successfully made a new campground');
-        res.redirect(`/campgrounds/${campground._id}`)
+    res.redirect(`/campgrounds/${campground._id}`)
 }
 
 module.exports.showCampground = async(req, res) => {
@@ -51,6 +66,8 @@ module.exports.updateCampground = async(req, res) => {
     const { id } = req.params;
     console.log(req.body);
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 });
+    campground.geometry = geoData.features[0].geometry;
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs);
     await campground.save();
---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,18 @@
+maptilersdk.config.apiKey = maptilerApiKey;
+
+const map = new maptilersdk.Map({
+  container: 'map',
+  style: maptilersdk.MapStyle.STREETS,
+  center: [campground.geometry.coordinates[0], campground.geometry.coordinates[1]],
+  zoom: 10
+});
+
+new maptilersdk.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new maptilersdk.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
+            )
+    )
+    .addTo(map)
