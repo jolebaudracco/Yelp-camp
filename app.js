ac72519072a852bdba15c7ef3ec76cec9fa0f88d
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-05-28 14:18:37 -0300

    making second deployment

--- app.js
+++ app.js
@@ -23,10 +23,14 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
-mongoose.connect(dbUrl) 
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
@@ -39,7 +43,6 @@ const app = express();
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-// console.log('Views directory:', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
